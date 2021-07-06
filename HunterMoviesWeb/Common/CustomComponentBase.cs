202a8edc2df016144b44c58f1204d4f3ada20b6d
DESKTOP-HVS9PQD\emili <emiliotg@outlook.com>
2021-07-06 15:49:45 -0400

    changes

---
+++ HunterMoviesWeb/Common/CustomComponentBase.cs
@@ -0,0 +1,28 @@
+﻿using alvamaidsbackend.Common;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace alvamaidsbackend
+{
+    public class CustomComponentBase : ComponentBase
+    {
+        
+        
+        public bool IsBusy { get; set; }
+        public Helper.ComponentState ComponentState { get; set; } = Helper.ComponentState.Empty;
+        [Parameter]
+        public int Quantity { get; set; } = 10;
+        [Parameter]
+        public int QuantityOfPages { get; set; } = 1;
+
+        [Parameter]
+        public int Page { get; set; } = 0;
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+
+    }
+}
---
+++ HunterMoviesWeb/Common/CustomHttpClient.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace HunterMoviesWeb.Common
+{
+    public class CustomHttpClient : HttpClient
+    {
+        public async Task<T> GetJsonAsync<T>(string requestUri)
+        {
+            Debug.WriteLine("QUERY ========");
+            Debug.WriteLine(this.BaseAddress+requestUri);
+            var httpContent = await this.GetAsync(requestUri);
+            string jsonContent = await httpContent.Content.ReadAsStringAsync();
+
+            T obj = JsonConvert.DeserializeObject<T>(jsonContent);
+            Debug.WriteLine(obj);
+            httpContent.Dispose();
+            this.Dispose();
+            return obj;
+        }
+
+        public async Task<HttpResponseMessage> PostJsonAsync<T>(string requestUri, T content)
+        {
+            string myContent = JsonConvert.SerializeObject(content);
+            StringContent stringContent = new StringContent(myContent, Encoding.UTF8, "application/json");
+            var response = await this.PostAsync(requestUri, stringContent);
+            this.Dispose();
+            return response;
+        }
+
+        public async Task<HttpResponseMessage> PutJsonAsync<T>(string requestUri, T content)
+        {
+            string myContent = JsonConvert.SerializeObject(content);
+            StringContent stringContent = new StringContent(myContent, Encoding.UTF8, "application/json");
+            var response = await this.PutAsync(requestUri, stringContent);
+            this.Dispose();
+            return response;
+        }
+    }
+}
