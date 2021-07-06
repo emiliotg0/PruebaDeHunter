8c7f5c050ed773f62d738a0081bf2a831475571f
DESKTOP-HVS9PQD\emili <emiliotg@outlook.com>
2021-07-06 16:02:42 -0400

    Change 2

---
+++ HunterMoviesWeb/Controllers/ActorsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HunterMoviesWeb.Controllers
+{
+    public class ActorsController : Controller
+    {
+        // GET: ActorsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ HunterMoviesWeb/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HunterMoviesWeb.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: MoviesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
